561eac2620e58d9d238d42dac7f2ba5f85dff52c
nmesoma <nmesomasolo2809@gmail.com>
2024-10-03 18:39:22 +0200

    debuging task 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-from fabric.api import local
-from time import strftime
-
+from fabric import Connection
+from datetime import datetime
+import os
 
 def do_pack():
     """
-    Generates a .tgz file from the contents of the web_static folder
+    Generates a .tgz file from the contents of the web_static folder.
 
     Returns:
-        str: The file path of the generated .tgz file if successful else None.
+        str: The file path of the generated .tgz file if successful, else None.
     """
-
-    date_time = strftime("%Y%m%d%H%M%S")
+    
+    date_time = datetime.now().strftime("%Y%m%d%H%M%S")
     file_name = "versions/web_static_{}.tgz".format(date_time)
+    
     try:
-        local("mkdir -p versions")
-        local("tar -zcvf {} web_static".format(file_name))
+        os.makedirs("versions", exist_ok=True)  # Create versions directory if it doesn't exist
+        # Use Connection to execute local commands
+        conn = Connection("localhost")  # Using local machine for packing
+        
+        conn.run("tar -zcvf {} web_static".format(file_name))
         return file_name
     except Exception as err:
+        print(f"Error occurred: {err}")
         return None
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,29 @@
 #!/usr/bin/python3
-"""Deploy web static to different servers"""
-import re
-from fabric import Connection
-from os.path import join, exists, splitext
+"""web server distribution"""
+from fabric.api import *
+import os.path
 
-env_hosts = ["54.89.197.58", "34.229.11.29"]
-user = "ubuntu"
-key_filename = '~/.ssh/id_rsa'
+env.user = 'ubuntu'
+env.hosts = ["54.89.197.58", "	34.229.11.29"]
+env.key_filename = "~/id_rsa"
 
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 def do_deploy(archive_path):
+    """distributes an archive to your web servers
     """
-    Deploy a compressed archive to a remote server.
-    Args:
-        archive_path (str): The path to the compressed archive.
-    Returns:
-        bool: True if the deployment is successful, False otherwise.
-    """
-
-    if not exists(archive_path):
+    if os.path.exists(archive_path) is False:
         return False
-
-    for host in env_hosts:
-        try:
-            conn = Connection(
-                host=host, user=user, connect_kwargs={"key_filename": key_filename}
-            )
-            conn.put(archive_path, "/tmp/")
-            file_name = re.search(r'[^/]+$', archive_path).group(0)
-            deploy_path = join("/data/web_static/releases/",
-                               splitext(file_name)[0])
-            conn.sudo("mkdir -p {}".format(deploy_path))
-
-            conn.sudo("tar -xzf /tmp/{} -C {}".format(file_name, deploy_path))
-
-            with conn.cd(deploy_path):
-                conn.sudo("mv hbnb_static/* .")  # Update to hbnb_static
-                conn.sudo("rm -rf hbnb_static")   # Update to hbnb_static
-
-            conn.sudo("rm /tmp/{}".format(file_name))
-            conn.sudo("rm -rf /data/hbnb_static/current")
-
-            conn.sudo('ln -sf {} /data/hbnb_static/current'.format(deploy_path))
-
-        except Exception as err:
-            print(f"Deployment failed on {host}: {err}")
-            return False
-
-    return True
+    try:
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
+        put(archive_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
+        return True
+    except:
+        return False
